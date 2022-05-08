b113e4686ad97e7233c8c6390af95fe24c3fb176
illinia <seolin1218@gmail.com>
2022-05-08 21:12:43 +0900

    테스트 코드 작성, 환경설정

---
+++ src/main/java/com/springboot/web/HelloController.java
@@ -0,0 +1,21 @@
+package com.springboot.web;
+
+import com.springboot.web.dto.HelloResponseDto;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+
+    @GetMapping("/hello/dto")
+    public HelloResponseDto helloDto(@RequestParam("name") String name,
+                                     @RequestParam("amount") int amount) {
+        System.out.println(new HelloResponseDto(name, amount));
+        return new HelloResponseDto(name, amount);
+    }
+}
